35b7fa6d63963feed5a9f1e63e856b19eda29899
tian <tian_chaiya12@hotmail.com>
2017-10-31 19:57:47 +0700

    set up basic file structure

---
+++ src/Components/ObjectComponent/ObjectComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ObjectComponent extends Component {
+  render() {
+    return (
+      <div className="ObjectComponent">
+        Object Component
+      </div>
+    );
+  }
+}
+
+export default ObjectComponent;
