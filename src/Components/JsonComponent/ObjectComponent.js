1e8250d75fecc9b17568c6d8a508633c2cf6eddd
tian <tian_chaiya12@hotmail.com>
2017-11-06 02:59:33 +0700

    rearrage object and array component into one json component folder

--- src/Components/JsonComponent/ObjectComponent.js
+++ src/Components/JsonComponent/ObjectComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import shortid from 'shortid'
-import ObjectComponent from '../ObjectComponent/ObjectComponent';
+import ArrayComponent from '../JsonComponent/ArrayComponent';
 
-class ArrayComponent extends Component {
+class ObjectComponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -40,35 +40,67 @@ class ArrayComponent extends Component {
 
   render() {
     const data = this.props.data;
-    const dataLength = data.length
-    let detectObject = false
+    const elem = [];
+    const dataLength = Object.keys(data).length;
 
-    // loop through each element in array
-    const finalElem = data.map((element, index) => {
-      // if string/number/boolean, represent as such
-      if (typeof element === 'string' || typeof element === 'number' || typeof element === 'boolean') {
-        return (
-          <div key={shortid.generate()} style={{marginLeft: '5px', display: 'inline'}}>
-            <span>{`${element}${index + 1 !== dataLength ? ',' : ''}`}</span>
+    let counter = 0;
+    // loop through each key in object
+    for (const key in data) {
+      counter++
+      // if data is number/string/boolean, return normal {key: number/string}
+      if (typeof data[key] === 'string' || typeof data[key] === 'number' || typeof data[key] === 'boolean') {
+        elem.push(
+          <div key={shortid.generate()} style={{marginLeft: '15px'}}>
+            <span>{`${key}: ${data[key]}${counter !== dataLength ? ',' : ''}`}</span>
           </div>
         )
       }
-      // if array, call itself recursively data
-      else if (Object.prototype.toString.call(element) === "[object Object]") {
-        detectObject = true
-        const objectComp = (
-          <ObjectComponent
-            data={element}
+      // if data is array, call ArrayCo
+      // mponent
+      else if (Array.isArray(data[key])) {
+        const index = counter - 1
+        const arrayComp = (
+          <ArrayComponent
+            data={data[key]}
             nested={true}
-            commar={index + 1 !== dataLength ? true : false}
+            commar={counter !== dataLength ? true : false}
             componentIndex={index}
             hoistState={this.updateChildButtonsState}
             savedState={this.state.childBtnState.length >= 0 && this.state.childBtnState[index]}
           />
         )
-        return (
+        const returnElem = (
           <div key={shortid.generate()} style={{marginLeft: '15px'}}>
-            <span>{`{`}</span>
+            <span>{`${key}: [`}</span>
+              <span>
+                {this.state.ownBtnState[index] &&
+                  <span style={{float:'right'}} onClick={(e) => this.expandButton(e, index)}> {'<'} </span>}
+              </span>
+              <span>
+                {this.state.ownBtnState[index]
+                  ? arrayComp
+                  : <span onClick={(e) => this.expandButton(e, index)}>{'...]'}</span>}
+              </span>
+          </div>
+        )
+        elem.push(returnElem)
+      }
+      // if data is object, call ObjectComponent recursively
+      else if (Object.prototype.toString.call(data[key]) === "[object Object]") {
+        const index = counter - 1
+        const objectComp = (
+          <ObjectComponent
+            data={data[key]}
+            nested={true}
+            commar={counter !== dataLength ? true : false}
+            componentIndex={index}
+            hoistState={this.updateChildButtonsState}
+            savedState={this.state.childBtnState.length >= 0 && this.state.childBtnState[index]}
+          />
+        )
+        const returnElem = (
+          <div key={shortid.generate()} style={{marginLeft: '15px'}}>
+            <span>{`${key}: {`}</span>
             <span>
               {this.state.ownBtnState[index] &&
                 <span style={{float:'right'}} onClick={(e) => this.expandButton(e, index)}> {'<'} </span>}
@@ -76,62 +108,35 @@ class ArrayComponent extends Component {
             <span>
               {this.state.ownBtnState[index]
                 ? objectComp
-                : <span onClick={(e) => this.expandButton(e, index)}>{index + 1 !== dataLength ? '...},' : '...}'}</span>}
+                : <span onClick={(e) => this.expandButton(e, index)}>{counter !== dataLength ? '...},' : '...}'}</span>}
             </span>
           </div>
         )
-      }
-      // if object, call object and pass data
-      else if (Array.isArray(element)) {
-        detectObject = true
-        const arrayComp = (
-          <ArrayComponent
-            data={element}
-            nested={true}
-            commar={index + 1 !== dataLength ? true : false}
-            componentIndex={index}
-            hoistState={this.updateChildButtonsState}
-            savedState={this.state.childBtnState.length >= 0 && this.state.childBtnState[index]}
-          />
-        )
-        return (
-          <div key={shortid.generate()} style={{marginLeft: '15px'}}>
-            <span>{'['}</span>
-            <span>
-              {this.state.ownBtnState[index] &&
-                <span style={{float:'right'}} onClick={(e) => this.expandButton(e, index)}> {'<'} </span>}
-            </span>
-            <span>
-              {this.state.ownBtnState[index]
-                ? arrayComp
-                : <span onClick={(e) => this.expandButton(e, index)}>{index + 1 !== dataLength ? '...],' : '...]'}</span>}
-            </span>
-          </div>
-        )
+        elem.push(returnElem)
       }
       // otherwise, returns undefined
       else {
-        return (
-          <div key={shortid.generate()} style={{marginLeft: '5px'}}>
-            <span>{`undefined${index + 1 !== data.length ? ',' : ''}`}</span>
+        elem.push(
+          <div key={shortid.generate()} style={{marginLeft: '15px'}}>
+            <span>{`${key}: undefined${counter !== dataLength ? ',' : ''}`}</span>
           </div>
         )
       }
-    })
+    }
 
     return (
-      <div className="ArrayComponent" style={{ display: !detectObject && 'inline'}}>
-        { this.props.nested ? '' : '['}
-          {finalElem}
-        { this.props.commar ? '],' : ']'}
+      <div className="ObjectComponent">
+        { this.props.nested ? '' : '{'}
+          {elem}
+        { this.props.commar ? '},' : '}'}
       </div>
     );
   }
 }
 
 
-ArrayComponent.propTypes = {
-  data: PropTypes.array.isRequired
+ObjectComponent.propTypes = {
+  data: PropTypes.object.isRequired
 };
 
-export default ArrayComponent;
+export default ObjectComponent;
