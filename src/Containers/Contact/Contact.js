9ab98a31dd495691ffea2182cd314ee6ef76e227
tian <tian_chaiya12@hotmail.com>
2017-11-07 04:03:16 +0700

    load txt file of an ASCII photo and put into contact. Display of text works for safari chrome and firefox latest version. It has yet to be tested against other browser

--- src/Containers/Contact/Contact.js
+++ src/Containers/Contact/Contact.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import './Contact.css'
 import contactData from '../../data/contact.json'
+import profile from '../../data/profile-picture.txt'
 import ObjectComponent from '../../Components/JsonComponent/ObjectComponent'
 
 class Contact extends Component {
   constructor() {
     super()
     this.state = {
-      contact: contactData
+      contact: contactData,
+      profile: "initial",
     }
   }
+
+  componentDidMount() {
+    fetch(profile)
+      .then(response => response.text())
+      .then(text => this.setState({profile: text}))
+  }
+
   render() {
     return (
       <div className="container">
         <div className="contact">
           <div className="contact-box">
+            <h1>CONTACT</h1>
             <ObjectComponent data={this.state.contact} />
+            <p className='profile'>{this.state.profile}</p>
           </div>
         </div>
       </div>
