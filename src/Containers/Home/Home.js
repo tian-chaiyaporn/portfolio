66a7c2dabba960d136d727c92b01ae74e20780c1
tian <tian_chaiya12@hotmail.com>
2017-11-09 01:39:09 +0700

    add home component with metadata

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group';
+import './Home.css'
+import homeData from '../../data/metadata.json'
+import ObjectComponent from '../../Components/JsonComponent/ObjectComponent'
 
 class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: homeData,
+    }
+  }
+
   render() {
     return (
-      <div className="home">
-        Home
+      <div className="container">
+        <div className="home">
+          <div className="home-item">
+            <CSSTransitionGroup
+              transitionName="typing-heading-home"
+              transitionAppear={true}
+              transitionAppearTimeout={500}
+              transitionEnter={false}
+              transitionLeave={false}
+            >
+              <h1 className="heading">METADATA</h1>
+            </CSSTransitionGroup>
+            <CSSTransitionGroup
+              transitionName="typing-body-home"
+              transitionAppear={true}
+              transitionAppearTimeout={1000}
+              transitionEnter={false}
+              transitionLeave={false}
+            >
+              <ObjectComponent data={this.state.data} />
+            </CSSTransitionGroup>
+          </div>
+        </div>
       </div>
     );
   }
